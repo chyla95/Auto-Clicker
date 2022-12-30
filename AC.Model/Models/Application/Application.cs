4e1c0af7c2d8888d70ba75f748ca4073ed0f045a
chyla95 <chyla95@gmail.com>
2022-12-30 06:38:29 +0100

    Add Application-related models

---
+++ AC.Model/Models/Application/Application.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using AC.ProcessManager.Core;
+
+namespace AC.Model.Models.Application
+{
+    public class Application
+    {
+        private readonly Process _process;
+        private readonly IKeyboardEmulator _keyboardEmulator;
+
+        public int Id
+        {
+            get
+            {
+                return _process.Id;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _process.ProcessName;
+            }
+        }
+        public Window? Window
+        {
+            get
+            {
+                IntPtr windowHandle = GetWindowHandle();
+                if (windowHandle == IntPtr.Zero) return null;
+
+                return new Window(windowHandle, _keyboardEmulator);
+            }
+        }
+
+        public Application(Process process, IKeyboardEmulator keyboardEmulator)
+        {
+            _process = process;
+            _keyboardEmulator = keyboardEmulator;
+        }
+
+        private IntPtr GetWindowHandle()
+        {
+            IntPtr windowHandle = _process.MainWindowHandle;
+            return windowHandle;
+        }
+    }
+}
---
+++ AC.Model/Models/Application/Window.cs
@@ -0,0 +1,69 @@
+﻿using AC.ProcessManager.Core;
+using static PInvokeWrapper.Window.Window;
+
+namespace AC.Model.Models.Application
+{
+    public class Window
+    {
+        private readonly IntPtr _handle;
+        private readonly IKeyboardEmulator _keyboardEmulator;
+
+        public IntPtr Handle
+        {
+            get
+            {
+                return _handle;
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                string value = GetTextM(Handle);
+                return value;
+            }
+            set
+            {
+                SetTextM(Handle, value);
+            }
+        }
+        public List<Window> ChildWindows
+        {
+            get
+            {
+                return GetChildWindows().ToList();
+            }
+        }
+
+        public Window(IntPtr handle, IKeyboardEmulator keyboardEmulator)
+        {
+            _handle = handle;
+            _keyboardEmulator = keyboardEmulator;
+        }
+
+        private IEnumerable<Window> GetChildWindows()
+        {
+            IEnumerable<IntPtr> childHandles = GetChildWindowsHandlesM(Handle);
+            List<Window> childWindows = new();
+
+            foreach (IntPtr childHandle in childHandles)
+            {
+                Window childWindow = new(childHandle, _keyboardEmulator);
+                childWindows.Add(childWindow);
+            }
+
+            return childWindows;
+        }
+
+        public void PostKey(KeyId keyId, KeyAction keyAction)
+        {
+            KeyboardKey key = _keyboardEmulator.GetKey(keyId);
+            key.Post(Handle, keyAction);
+        }
+        public void SendKey(KeyId keyId, KeyAction keyAction)
+        {
+            KeyboardKey key = _keyboardEmulator.GetKey(keyId);
+            key.Send(Handle, keyAction);
+        }
+    }
+}
