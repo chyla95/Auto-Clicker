cb83031ea2af59dd376aac871c8a4c20a689d29b
chyla95 <chyla95@gmail.com>
2023-01-03 18:05:02 +0100

    Disable controls on app start

---
+++ AC.View/Converters/BooleanInversionConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.UI.Xaml.Data;
+
+namespace AC.View.Converters
+{
+    public class BooleanInversionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
